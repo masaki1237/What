0b24b2ef596256c25b810afcac7e5f4ad6c5f233
MASAKI <masaki_325@icloud.com>
2021-08-28 19:24:55 -0700

    storyboardにデザインを加えました

---
+++ What/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  What
+//
+//  Created by 松本昌樹 on 2021/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet var button: UIButton!
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ What/ViewController3.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController3.swift
+//  What
+//
+//  Created by 松本昌樹 on 2021/08/08.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    @IBOutlet var label2: UILabel!
+    @IBOutlet var label3: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        label.isHidden = true
+        label2.isHidden = true
+        label3.isHidden = true
+    }
+        
+        @IBAction func tapButton1 () {
+            label.isHidden = false
+            }
+    @IBAction func tapButton2 () {
+        label2.isHidden = false
+    }
+    @IBAction func tapButton3 () {
+        label3.isHidden = false
+    }
+    @IBAction func back () {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func delete () {
+        label.isHidden = true
+        label2.isHidden = true
+        label3.isHidden = true
+    }
+    
+
+        // Do any additional setup after loading the view.
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
